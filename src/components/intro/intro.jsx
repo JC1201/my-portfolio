a7a8ee9d434a6e0b600e375dd65f55b7b901c855
JC1201 <jiehchen@lancaster.ac.uk>
2025-08-24 20:58:27 +0100

    added amimation text type on intro part

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,37 +1,41 @@
 import './intro.css';
 import personalPic from '../../assets/personal_pic.png';
+import { motion } from 'framer-motion';
 
 export function Intro() {
 
-  
+  const introText = "I'm a passionate graduated Computer Science student from Malaysia 🇲🇾";
+
   return (
-    
     <section className="intro">
-
       <div className='intro-content'>
-      <div className="intro-image">
-        <img src={personalPic} alt="Jieh Cheng" />
-      </div>
-
-      <div className = "intro-desc">
-          
-        <div className="intro-text">
-          <h1>
-            Hello, I'm <span>Jieh Cheng</span> 👋
-          </h1>
-          <p>
-            I'm a passionate graduated Computer Science student from Malaysia 🇲🇾
-          </p>
-             <a href="#contact" onClick={() => setActive('contact')}>Contact Me</a>
-
+        <div className="intro-image">
+          <img src={personalPic} alt="Jieh Cheng" />
         </div>
 
+        <div className="intro-desc">
+          <div className="intro-text">
+            <h1>
+              Hello, I'm <span>Jieh Cheng</span> 👋
+            </h1>
+
+            <p className="typing-text">
+              {introText.split("").map((char, index) => (
+                <motion.span
+                  key={index}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: index * 0.05 }}
+                >
+                  {char}
+                </motion.span>
+              ))}
+            </p>
+
+            <a href="#contact">Contact Me</a>
+          </div>
+        </div>
       </div>
-
-      </div>
-
-      
-
     </section>
   );
 }
